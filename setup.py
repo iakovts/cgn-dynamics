8102a41d010c6f082d3f36a1303b8d9273742924
iakovos <itsouros@auth.gr>
2022-05-05 17:47:04 +0300

    Add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# from distutils.core import setup
+from setuptools import find_packages, setup
+
+reqs = ["numpy", 'dataclasses;python_version<"3.7"']
+# extra_reqs = {"pres": ["plotly", "jupyterlab"]}
+setup(
+    name="CGN",
+    version="1.0",
+    description="CGN w/ dynamics",
+    author="Iakovos Tsouros",
+    # packages=find_packages(include=["src*", "util*"]),
+    packages=find_packages(),
+    python_requires=">3.6.*",
+    install_requires=reqs,
+    entry_points={
+        "console_scripts": [
+            "mic=micwave.src.main:run",
+        ]
+    },
+    # extras_require=extra_reqs,
+)
