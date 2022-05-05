27ad9aae84fd2628586ac23df813eab3e74f9af3
iakovos <itsouros@auth.gr>
2022-05-05 17:47:28 +0300

    Add config and dynamics base classes

--- cgndyna/config/base.py
+++ cgndyna/config/base.py
@@ -1,6 +1,22 @@
+import networkx as nx
+
 from dataclasses import dataclass, field, make_dataclass
+
+from typing import Union
+
+
 
 
 @dataclass
+class Network:
+    name: str
+    nw_param: Union[int, float]  # Network parameter i.e.: `p` for GNP or `m` for BA
+    num_nodes: int = field(default=1000)
+    # is_modular: bool = field(default=False)
+
+    # def __post_init__(self):
+    #     if self.is_modular:
+
+@dataclass
 class Config:
-    pass
+    nw: Network = field(init=False)
--- cgndyna/util/dynamics/base.py
+++ cgndyna/util/dynamics/base.py
@@ -5,9 +5,12 @@ import numpy as np
 
 class Dynamics(ABC):
     def __init__(self, config=None):
-        self.config = config
+        self.cfg = config
 
     @abstractmethod
     def initial_state(self):
         pass
+
+    def get_neighbors_state(self):
+        self.neighbors = {}
     
