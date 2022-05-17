21f2278d82899069858061a0b979d07abb828257
iakovos <itsouros@auth.gr>
2022-05-17 16:03:29 +0300

    Fix Dynamics

--- cgndyna/src/experiment.py
+++ cgndyna/src/experiment.py
@@ -8,6 +8,7 @@ from cgndyna.util.dynamics.majority import MajorityRule
 from cgndyna.config.base import Config, ExperimentCfg, NetworkCfg, DynamicsCfg
 from cgndyna.dataset.dataset import Dataset
 from cgndyna.dataset.data_transformer import DatasetLoader
+from cgndyna.nn.models import train
 
 if TYPE_CHECKING:
     import torch_geometric_temporal as tg
@@ -48,10 +49,11 @@ class Experiment:
             self.set_dynamics(self.networks[nw])
             for sample in range(self.cfg.exp.num_samples):
                 x = self.dynamics.initial_state()
+                # import pdb; pdb.set_trace()
                 dataset.data[sample, :, 0] = x
                 for lagstep in range(1, self.cfg.exp.lag):
                     if lagstep % self.cfg.exp.lagstep == 0:
-                        dataset.data[sample, :, lagstep] = self.dynamics.step(x)
+                        dataset.data[sample, :, lagstep] = x = self.dynamics.step(x)
             self.dataset.append(dataset)
 
     def transform_data(self) -> None:
@@ -60,14 +62,14 @@ class Experiment:
             self.signals[nw_idx] = DatasetLoader(self.dataset, nw_idx).get_dataset()
 
 
-
 def test_only():
     c = Config()
     c.nw = NetworkCfg("gnp", 0.004)
-    c.exp = ExperimentCfg(lag=5, num_networks=5, num_samples=10)
+    c.exp = ExperimentCfg(lag=5, num_networks=5, num_samples=100)
     c.dyn = DynamicsCfg("major", [0.4, 0.6])
     e = Experiment(c)
     e.generate_data()
+    e.transform_data()
     return e
 
 
